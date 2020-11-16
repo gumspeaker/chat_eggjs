e5c85059020736bab94f21689bbea8fb681d4eae
xuzhihao <774910945@qq.com>
2020-11-16 21:59:40 +0800

    1

--- app/controller/user.js
+++ app/controller/user.js
@@ -1,28 +1,30 @@
-'use strict';
-
-const Controller = require('egg').Controller;
+const Controller = require("egg").Controller;
 
 class UserController extends Controller {
   async sign() {
-    const { ctx, service, } = this;
+    const { ctx, service } = this;
     let userInfo = ctx.request.body;
-    const signInformation = await ctx.service.user.addUser(userInfo)
-    ctx.helper.success(signInformation)
+    const signInformation = await ctx.service.user.addUser(userInfo);
+    ctx.helper.success(signInformation);
     // ctx.body = signInformation
   }
   async login() {
-    const {ctx, service, app, helper } = this;
-    let userInfo = ctx.request.body
-    const loginInformation = await ctx.service.user.login(userInfo)
+    const { ctx, service, app, helper } = this;
+    let userInfo = ctx.request.body;
+    const loginInformation = await ctx.service.user.login(userInfo);
     if (loginInformation == "error") {
-      ctx.body = "参数错误"
+      ctx.body = "参数错误";
     } else {
-      const token = await service.actionToken.apply(loginInformation.user_name)
-      ctx.helper.writeToken(loginInformation.user_name,token)
+      const token = await service.actionToken.apply(loginInformation.user_name);
+      ctx.helper.writeToken(loginInformation.user_name, token);
       // vo中不该有密码
-      delete loginInformation.user_password
-      ctx.helper.success({loginInformation,token})
+      delete loginInformation.user_password;
+      ctx.helper.success({ loginInformation, token });
     }
+  }
+  async loginOut() {
+    const { ctx, service } = this;
+    let username = ctx.request.body;
   }
 }
 
